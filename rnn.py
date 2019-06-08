0859831011070d6e79b822040ebf661fc458fe22
ThalissonMelo <tallisonmelo46@gmail.com>
2019-06-08 11:41:32 -0300

    Changing LSTM to GRU

--- rnn.py
+++ rnn.py
@@ -34,29 +34,29 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 # Part 2 - Building the RNN
 
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import GRU
+from tensorflow.keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
 
 # Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.1))
 
 # Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 50, return_sequences = True))
+regressor.add(Dropout(0.1))
 
 # Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 50, return_sequences = True))
+regressor.add(Dropout(0.1))
 
 # Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 50))
+regressor.add(Dropout(0.1))
 
 # Adding the output layer
 regressor.add(Dense(units = 1))
@@ -68,7 +68,7 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 
-
+    
 # Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
