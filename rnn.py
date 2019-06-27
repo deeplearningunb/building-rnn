b604f9d7cdcd4c431a748f4901ee059d5b67b5eb
CaioNunes <caiofelipe147@gmail.com>
2019-06-27 10:53:38 -0300

    Create RNN using GRU

--- rnn.py
+++ rnn.py
@@ -1,20 +1,23 @@
 # Recurrent Neural Network
 
-
-
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import GRU
+from keras.layers import Dropout
+import pickle
 
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
@@ -29,34 +32,22 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-
-
 # Part 2 - Building the RNN
-
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 100, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.1))
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 100, return_sequences = True))
+regressor.add(Dropout(0.1))
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 100, return_sequences = True))
+regressor.add(Dropout(0.1))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+regressor.add(GRU(units = 100))
+regressor.add(Dropout(0.1))
 
 # Adding the output layer
 regressor.add(Dense(units = 1))
@@ -68,6 +59,17 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 
+# ========= Save Model ===============
+filename = 'training_googleprices_savemodel.sav'
+file = open(filename, 'wb')
+pickle.dump(regressor, file)
+
+file.close()
+
+# ==== Load model ====
+
+file = open(filename, 'rb')
+loaded_model = pickle.load(file)
 
 # Part 3 - Making the predictions and visualising the results
 
