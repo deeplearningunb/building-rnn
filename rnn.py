9be1f2d2987dc2ad02158adcd9c9b23eb29c2aae
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-11-24 23:33:59 -0300

    feat(rnn): evolve rnn
    - Change layer from LSTM to GRU
    - Update optimizer
    - Add metrics
    - Other adjustments
    - Black style applied
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- rnn.py
+++ rnn.py
@@ -1,5 +1,3 @@
-# Recurrent Neural Network
-
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
@@ -8,89 +6,100 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+dataset_train = pd.read_csv("Google_Stock_Price_Train.csv")
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+
+sc = MinMaxScaler(feature_range=(0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
 # Creating a data structure with 60 timesteps and 1 output
-X_train = []
+x_train = []
 y_train = []
 for i in range(60, 1258):
-    X_train.append(training_set_scaled[i-60:i, 0])
+    x_train.append(training_set_scaled[i - 60 : i, 0])
     y_train.append(training_set_scaled[i, 0])
-X_train, y_train = np.array(X_train), np.array(y_train)
+x_train, y_train = np.array(x_train), np.array(y_train)
 
 # Reshaping
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-
-
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+print(x_train)
 
 # Part 2 - Building the RNN
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
 from keras.layers import Dropout
+from keras.layers import GRU
 
 # Initialising the RNN
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+# Adding the first GRU layer and some Dropout regularisation
+regressor.add(
+    GRU(
+        units=50,
+        activation="tanh",
+        recurrent_activation="sigmoid",
+        use_bias=True,
+        return_sequences=True,
+        input_shape=(x_train.shape[1], 1),
+    )
+)
 regressor.add(Dropout(0.2))
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+# Adding the second GRU layer and some Dropout regularisation
+regressor.add(GRU(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+# Adding the third GRU layer and some Dropout regularisation
+regressor.add(GRU(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
+# Adding the fourth GRU layer and some Dropout regularisation
+regressor.add(GRU(units=50,))
 regressor.add(Dropout(0.2))
 
 # Adding the output layer
-regressor.add(Dense(units = 1))
+regressor.add(Dense(units=1))
 
 # Compiling the RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.compile(
+    optimizer="nadam",
+    loss="mean_squared_error",
+    metrics=["accuracy", "mse"],
+)
 
 # Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
-
-
+regressor.fit(x_train, y_train, epochs=50, batch_size=10)
 
 # Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+dataset_test = pd.read_csv("Google_Stock_Price_Test.csv")
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of 2017
-dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = inputs.reshape(-1,1)
+dataset_total = pd.concat((dataset_train["Open"], dataset_test["Open"]), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60 :].values
+inputs = inputs.reshape(-1, 1)
 inputs = sc.transform(inputs)
-X_test = []
+x_test = []
 for i in range(60, 80):
-    X_test.append(inputs[i-60:i, 0])
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-predicted_stock_price = regressor.predict(X_test)
+    x_test.append(inputs[i - 60 : i, 0])
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+predicted_stock_price = regressor.predict(x_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualising the results
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
-plt.title('Google Stock Price Prediction')
-plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
+plt.plot(real_stock_price, color="red", label="Real Google Stock Price")
+plt.plot(predicted_stock_price, color="blue", label="Predicted Google Stock Price")
+plt.title("Google Stock Price Prediction")
+plt.xlabel("Time")
+plt.ylabel("Google Stock Price")
 plt.legend()
 plt.show()
