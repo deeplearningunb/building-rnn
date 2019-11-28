7941d3005df8b5d78064974234b544fcb560e413
Caio Fernandes <caiovf13@gmail.com>
2019-11-27 21:28:55 -0300

    Change LSTM layer to GRU

--- rnn.py
+++ rnn.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+dataset_train = pd.read_csv('/Users/caiofernandes/Desktop/Projetos/DeepLearning/RecurrentNeuralNetwork/building-rnn/Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
@@ -34,26 +34,26 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import GRU
 from keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+# Adding the first GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+# Adding a second GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+# Adding a third GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
+# Adding a fourth GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 50))
 regressor.add(Dropout(0.2))
 
 # Adding the output layer
@@ -70,7 +70,7 @@ regressor.fit(X_train, y_train, epochs = 10, batch_size = 32)
 # Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+dataset_test = pd.read_csv('/Users/caiofernandes/Desktop/Projetos/DeepLearning/RecurrentNeuralNetwork/building-rnn/Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of 2017
