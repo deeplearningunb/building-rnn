5f5234e45241fa01ad458a0d9f4641fd68979243
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-10-03 15:42:38 -0300

    Ajustes nos hyper parametros e alteracao da camada recorrente

--- rnn.py
+++ rnn.py
@@ -19,8 +19,8 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 1258):
-    X_train.append(training_set_scaled[i-60:i, 0])
+for i in range(40, 1258):
+    X_train.append(training_set_scaled[i-40:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
@@ -34,27 +34,27 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 # Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import GRU
 from keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
 
-# Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
+# Adding the first GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 30, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.1))
 
-# Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+# Adding a second GRU layer and some Dropout regularisation
+# regressor.add(GRU(units = 60, return_sequences = True))
+# regressor.add(Dropout(0.1))
 
-# Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+# Adding a third GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 90, return_sequences = True))
+# regressor.add(Dropout(0.1))
 
-# Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+# Adding a fourth GRU layer and some Dropout regularisation
+regressor.add(GRU(units = 60))
+regressor.add(Dropout(0.1))
 
 # Adding the output layer
 regressor.add(Dense(units = 1))
@@ -75,12 +75,12 @@ real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of 2017
 dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 40:].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 X_test = []
-for i in range(60, 80):
-    X_test.append(inputs[i-60:i, 0])
+for i in range(40, 60):
+    X_test.append(inputs[i-40:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = regressor.predict(X_test)
