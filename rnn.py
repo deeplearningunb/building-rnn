627de950b70e552d15889846318e3d7767c4989e
jpmartins201 <jpmartins201@live.com>
2021-05-15 10:53:36 -0300

    Adding hyperparameters and layer changes

--- rnn.py
+++ rnn.py
@@ -35,25 +35,26 @@ X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import GRU
 from keras.layers import Dropout
 
 # Initialising the RNN
 regressor = Sequential()
 
 # Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(GRU(units=30, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
 
 # Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(GRU(units = 30, return_sequences = True))
 regressor.add(Dropout(0.2))
 
 # Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(GRU(units = 30, return_sequences = True))
 regressor.add(Dropout(0.2))
 
 # Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
+regressor.add(GRU(units = 30))
 regressor.add(Dropout(0.2))
 
 # Adding the output layer
